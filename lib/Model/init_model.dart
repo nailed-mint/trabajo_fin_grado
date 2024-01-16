b53546baf86b64e081c3bbefa2005abcfa79b81a
nailed-mint <nailedmint@gmail.com>
2024-01-16 20:45:05 +0100

    [ADD] global model file

---
+++ lib/Model/init_model.dart
@@ -0,0 +1,7 @@
+export 'package:trabajo_fin_grado/Model/child.dart';
+export 'package:trabajo_fin_grado/Model/game.dart';
+export 'package:trabajo_fin_grado/Model/professional.dart';
+export 'package:trabajo_fin_grado/Model/report.dart';
+export 'package:trabajo_fin_grado/Model/result.dart';
+export 'package:trabajo_fin_grado/Model/gameplay.dart';
+export 'package:trabajo_fin_grado/Model/user.dart';
