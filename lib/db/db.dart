7bca299ea45808e3491828aa9574b8c2a4d78ce6
nailed-mint <nailedmint@gmail.com>
2024-01-17 21:04:34 +0100

    [MOD] table independent methods

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -18,39 +18,90 @@ class DB {
     );
   }
 
-  static Future<void> insert(String tableName, dynamic item) async {
+  // CRUD ======================================================================
+  static Future<void> insert({
+    required String table,
+    required Map<String, dynamic> item,
+  }) async {
     Database database = await _openDB();
 
-    await database.insert(tableName, item.toMap());
+    await database.insert(table, item);
   }
 
-  static Future<void> delete(String tableName, dynamic item) async {
+  static Future<List<Map<String, dynamic>>> list({
+    required String table,
+    Map<String, dynamic>? filters,
+    int? limit,
+  }) async {
+    Map<String, dynamic> mappedFilters = _mapFilters(filters: filters);
+
     Database database = await _openDB();
 
-    await database.delete(tableName, where: "id = ?", whereArgs: [item.id]);
-  }
-
-  static Future<void> update(String tableName, dynamic item) async {
-    Database database = await _openDB();
-
-    await database.update(
-      tableName,
-      item.toMap(),
-      where: "id = ?",
-      whereArgs: [item.id],
+    return database.query(
+      table,
+      where: mappedFilters["where"],
+      whereArgs: mappedFilters["whereArgs"],
+      limit: limit,
     );
   }
 
-  static Future<List<Map<String, dynamic>>> list(
-    String tableName,
-    Type type,
-  ) async {
+  static Future<void> update({
+    required String table,
+    required Map<String, dynamic> values,
+    Map<String, dynamic>? filters,
+  }) async {
+    Map<String, dynamic> mappedFilters = _mapFilters(filters: filters);
+
     Database database = await _openDB();
 
-    return database.query(tableName);
+    await database.update(
+      table,
+      values,
+      where: mappedFilters["where"],
+      whereArgs: mappedFilters["whereArgs"],
+    );
   }
 
-  // TODO: encontrar una manera de mapear los filtros
-  // Así, podría mejorar el método list para conseguir subsets de las tablas
-  // Además, me permitiría implementar un método get genérico para todo
+  static Future<void> delete({
+    required String table,
+    Map<String, dynamic>? filters,
+  }) async {
+    Map<String, dynamic> mappedFilters = _mapFilters(filters: filters);
+
+    Database database = await _openDB();
+
+    await database.delete(
+      table,
+      where: mappedFilters["where"],
+      whereArgs: mappedFilters["whereArgs"],
+    );
+  }
+
+  // QOL METHODS ===============================================================
+  static Map<String, dynamic> _mapFilters({Map<String, dynamic>? filters}) {
+    Map<String, dynamic> res = {
+      "where": "",
+      "whereArgs": [],
+    };
+
+    if (filters == null) {
+      return res;
+    }
+
+    List<String> where = [];
+
+    for (var entry in filters.entries) {
+      String key = entry.key;
+      dynamic value = entry.value;
+      where.add("$key = ?");
+      res["whereArgs"]!.add(value);
+    }
+
+    res["where"] = where.join("AND");
+
+    return res;
+  }
+
+  // TODO: metodo para revisar la integridad de los datos antes de insertarlos
+  // en la db
 }
