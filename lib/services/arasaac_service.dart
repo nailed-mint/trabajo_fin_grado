58ff25da0a5be0cb7bdcbb0cac75df5bcfed6b61
nailed-mint <nailedmint@gmail.com>
2024-05-09 17:24:11 +0200

    ADD Format width arasaaac images

--- lib/services/arasaac_service.dart
+++ lib/services/arasaac_service.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 class ArasaacService {
   static const String baseUrl = "https://api.arasaac.org/api/pictograms/";
 
-  static Image getPictogram({required int icon}) {
+  static Image getPictogram({required int icon, double? width}) {
     return Image(
       image: NetworkImage(baseUrl + icon.toString()),
       fit: BoxFit.contain,
+      width: width,
     );
   }
 }
